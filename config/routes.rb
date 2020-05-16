22178516b0e45fdb28dc557ba79d7b5b83420ddf
jenyau <jekharitonov@gmail.com>
2020-05-16 20:36:15 +0300

    go

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   end
   get 'welcome/index'
 
-
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
