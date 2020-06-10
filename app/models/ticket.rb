4e13119198d4f481a3d2c5d4324d6c58f89e5ad2
jenyau <jekharitonov@gmail.com>
2020-06-10 18:13:39 +0300

    lesson19 before deploy

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,6 @@ class Ticket < ApplicationRecord
   #validates :passenger_name, presence: true
   belongs_to :train
   belongs_to :user
+  belongs_to :start_station, class_name: "RailwayStation", foreign_key: :start_station_id
+  belongs_to :end_station, class_name: "RailwayStation", foreign_key: :end_station_id
 end
--- db/migrate/20200401142906_add_routes_name_to_tickets.rb
+++ db/migrate/20200401142906_add_routes_name_to_tickets.rb
@@ -1,5 +1,5 @@
 class AddRoutesNameToTickets < ActiveRecord::Migration[6.0]
   def change
-    add_column :tickets, :routes_name, :string
+   # add_column :tickets, :routes_name, :string
   end
 end
